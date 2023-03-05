767d077fe1564540012a5126e21bf9e42196501b
Myprofilee <andrep13.id@gmail.com>
2023-03-05 19:36:30 +0700

    first commit

---
+++ assets/js/produk/game/myzuu.js
@@ -0,0 +1,570 @@
+// LINK TOKO
+const urlToko = "https://myzuu.bukaolshop.site"
+
+// AKTIVASI
+const aktivasi = "myzuu"
+
+
+// LINK Kategori
+const urlKategori = [
+  {
+    "nama": "Mobile Legends",
+    "cekID": "mobilelegends",
+    "gambar": "https://sc.myzuu.my.id/assets/img/produk/game/mobile-legend/myzuu.jpg",
+    "kode": "2InputGame",
+    "categoryKe1": {
+      "nama": "Diamond",
+      "url": "umum-412544",
+      "data": "grid"
+    },
+    "categoryKe2": {
+      "nama": "Membership",
+      "url": "membership-412546",
+      "data": "list"
+    }
+  },
+
+  /**  Kategori Game /
+  {
+    "nama": "Free Fire",
+    "cekID": "freefire",
+    "gambar": "https://i.ibb.co/2YZNmnm/free-fire-new-logo.jpg",
+    "kode": "1InputGame",
+    "categoryKe1": {
+      "nama": "Diamond",
+      "url": "free-fire-411855",
+      "data": "grid"
+    },
+    "categoryKe2": {
+      "nama": "#",
+      "url": "#",
+      "data": "list"
+    }
+  },
+
+  {
+    "nama": "PUBG",
+    "cekID": "#",
+    "gambar": "https://i.ibb.co/qCnVj9G/pubgm.jpg",
+    "kode": "1InputGame",
+    "categoryKe1": {
+      "nama": "UC Pubgm",
+      "url": "pubg-mobile-412042",
+      "data": "grid"
+    },
+    "categoryKe2": {
+      "nama": "Membership",
+      "url": "telkomsel-reguler-349764",
+      "data": "list"
+    }
+  },
+
+  {
+    "nama": "Arena Of Valor",
+    "cekID": "aov",
+    "gambar": "https://i.ibb.co/Xzg6VHS/aov-promo.jpg",
+    "kode": "1InputGame",
+    "categoryKe1": {
+      "nama": "Diamond",
+      "url": "telkomsel-transfer-350436",
+      "data": "grid"
+    },
+    "categoryKe2": {
+      "nama": "#",
+      "url": "#",
+      "data": "list"
+    }
+  },
+
+  {
+    "nama": "COD Mobile",
+    "cekID": "callofduty",
+    "gambar": "https://i.ibb.co/YTWK4cC/codm.jpg",
+    "kode": "1InputGame",
+    "categoryKe1": {
+      "nama": "CP",
+      "url": "codm-412060",
+      "data": "grid"
+    },
+    "categoryKe2": {
+      "nama": "#",
+      "url": "#",
+      "data": "list"
+    }
+  },
+
+  {
+    "nama": "Garena Shell",
+    "cekID": "#",
+    "gambar": "https://i.ibb.co/4JzkVr8/garena.jpg",
+    "kode": "1InputGame",
+    "categoryKe1": {
+      "nama": "Diamond",
+      "url": "telkomsel-transfer-350436",
+      "data": "grid"
+    },
+    "categoryKe2": {
+      "nama": "#",
+      "url": "#",
+      "data": "list"
+    }
+  },
+
+
+  {
+    "nama": "Point Blank",
+    "cekID": "pointblank",
+    "gambar": "https://i.ibb.co/Pjgt9nk/Point-Blank-ID-tile.jpg",
+    "kode": "1InputGame",
+    "categoryKe1": {
+      "nama": "PB Cash",
+      "url": "point-blank-412034",
+      "data": "grid"
+    },
+    "categoryKe2": {
+      "nama": "#",
+      "url": "#",
+      "data": "list"
+    }
+  },
+
+
+  {
+    "nama": "Hago",
+    "cekID": "hago",
+    "gambar": "https://i.ibb.co/j3HsywT/hagos.png",
+    "kode": "1InputGame",
+    "categoryKe1": {
+      "nama": "Diamond",
+      "url": "telkomsel-transfer-350436",
+      "data": "grid"
+    },
+    "categoryKe2": {
+      "nama": "#",
+      "url": "#",
+      "data": "list"
+    }
+  },
+
+  {
+    "nama": "AU2 Mobile",
+    "cekID": "au2m",
+    "gambar": "https://i.ibb.co/rZwNnkB/au2m.png",
+    "kode": "1InputGame",
+    "categoryKe1": {
+      "nama": "Diamond",
+      "url": "telkomsel-transfer-350436",
+      "data": "grid"
+    },
+    "categoryKe2": {
+      "nama": "#",
+      "url": "#",
+      "data": "list"
+    }
+  },
+
+  {
+    "nama": "Lost Saga",
+    "cekID": "#",
+    "gambar": "https://i.ibb.co/pn4gL4j/lost-saga-origin.jpg",
+    "kode": "1InputGame",
+    "categoryKe1": {
+      "nama": "Diamond",
+      "url": "telkomsel-transfer-350436",
+      "data": "grid"
+    },
+    "categoryKe2": {
+      "nama": "#",
+      "url": "#",
+      "data": "list"
+    }
+  },
+
+  {
+    "nama": "Genshin Impact",
+    "cekID": "gensin",
+    "gambar": "https://i.ibb.co/NrS3zLJ/genshin.jpg",
+    "kode": "genshinGame",
+    "categoryKe1": {
+      "nama": "Diamond",
+      "url": "genshin-impact-411842",
+      "data": "grid"
+    },
+    "categoryKe2": {
+      "nama": "Membership",
+      "url": "membership-gi-411844",
+      "data": "list"
+    }
+  },
+
+
+  {
+    "nama": "Valorant",
+    "cekID": "valorant",
+    "gambar": "https://i.ibb.co/rvpVvLd/valorant-promo.jpg",
+    "kode": "pagarGame",
+    "categoryKe1": {
+      "nama": "Diamond",
+      "url": "telkomsel-transfer-350436",
+      "data": "grid"
+    },
+    "categoryKe2": {
+      "nama": "#",
+      "url": "#",
+      "data": "list"
+    }
+  },
+
+  {
+    "nama": "Ragnarok Mobile",
+    "cekID": "ragnarokm",
+    "gambar": "https://i.ibb.co/WnpvQJ5/ragnarok-logo.webp",
+    "kode": "ragnarokGame",
+    "categoryKe1": {
+      "nama": "Diamond",
+      "url": "telkomsel-transfer-350436",
+      "data": "grid"
+    },
+    "categoryKe2": {
+      "nama": "#",
+      "url": "#",
+      "data": "list"
+    }
+  },
+  /**  Kategori Game **/
+
+]
+
+
+//---------- Jangan Dirubah ----------//
+
+// Loading
+var loadAwal = `<div id="loads" class="loading">
+<div class="loadingspinner">
+<div id="square1"></div>
+<div id="square2"></div>
+<div id="square3"></div>
+<div id="square4"></div>
+<div id="square5"></div>
+</div>
+<p id="text-load" class="fw-bold mt-2 text-center t-load">Masukan nomor yang valid terlebih dahulu...</p>
+</div>`
+
+var loads = `<div id="loads-2" class="loading">
+<div class="loadingspinner">
+<div id="square1"></div>
+<div id="square2"></div>
+<div id="square3"></div>
+<div id="square4"></div>
+<div id="square5"></div>
+</div>
+</div>`
+
+var noSalah = `<div id="no-salah" class="container text-center produk-none animate__animated animate__fadeIn">
+<img src="https://i.ibb.co/f8gVVGD/eror.jpg">
+<p class="fw-bold mt-3">Nomor yang dimasukan salah nih...</p>
+</div>`
+
+var produkNone = `<div id="produk-none" class="text-center container produk-none animate__animated animate__fadeIn">
+<img src="https://i.ibb.co/VwtjssF/digital-marketing.webp">
+<p class="fw-bold mt-3">Sepertinya produk ini belum tersedia...</p>
+</div>`
+
+var kategoriNone = `<div id="produk-none" class="text-center mt-3 container produk-none animate__animated animate__fadeIn">
+<img src="https://i.ibb.co/VwtjssF/digital-marketing.webp">
+<p class="fw-bold mt-3">Kategori belum tersedia...</p>
+</div>`
+
+//---------- Data API ----------//
+
+
+// loopSUbKat
+loopSubKat(urlKategori)
+function loopSubKat(data) {
+  $.each(data, function (i, v) {
+    $("#katUtama").append(`<div class="col-4 kerd text-center" style="margin-bottom:30px" onclick="hideInput('${v.kode}');loopDataKat('${v.categoryKe1.nama}', '${v.categoryKe1.url}', '${v.categoryKe1.data}', '${v.categoryKe2.nama}', '${v.categoryKe2.url}', '${v.categoryKe2.data}', '${v.cekID}', '${v.nama}', '${v.gambar}', '${v.kode}'); getData('${v.categoryKe1.url}', '${v.categoryKe1.nama}', '${v.categoryKe1.data}', '${v.cekID}', '${v.kode}')" data-bs-dismiss="offcanvas" aria-label="Close"">
+      <img src="${v.gambar}" class="image-game">
+      <p class="mt-1 fw-bold text-game">${v.nama}</p>
+      </div>`)
+  })
+
+}
+
+// hideInput
+var codeGamez = urlKategori[0].kode
+hideInput(codeGamez)
+function hideInput(kode) {
+  if (kode == "pagarGame") {
+    $("#box-fourth-input").addClass("d-none")
+    $("#box-third-input").addClass("d-none")
+    $("#box-sec-input").addClass("d-none")
+  } else if (kode == "1InputGame") {
+    $("#box-fourth-input").addClass("d-none")
+    $("#box-third-input").addClass("d-none")
+    $("#box-sec-input").addClass("d-none")
+  } else if (kode == "2InputGame") {
+    $("#box-fourth-input").addClass("d-none")
+    $("#box-third-input").addClass("d-none")
+    $("#box-sec-input").removeClass("d-none")
+  } else if (kode == "genshinGame") {
+    $("#box-fourth-input").addClass("d-none")
+    $("#box-third-input").removeClass("d-none")
+    $("#box-sec-input").addClass("d-none")
+  } else if (kode == "ragnarokGame") {
+    $("#box-fourth-input").removeClass("d-none")
+    $("#box-third-input").addClass("d-none")
+    $("#box-sec-input").addClass("d-none")
+  }
+}
+
+
+// LoopDataKat
+var kate1 = urlKategori[0].categoryKe1.nama
+var urle1 = urlKategori[0].categoryKe1.url
+var bentuke1 = urlKategori[0].categoryKe1.data
+var kate2 = urlKategori[0].categoryKe2.nama
+var urle2 = urlKategori[0].categoryKe2.url
+var bentuke2 = urlKategori[0].categoryKe2.data
+var cekIDe = urlKategori[0].cekID
+var namae = urlKategori[0].nama
+var gambare = urlKategori[0].gambar
+var codeGames = urlKategori[0].kode
+loopDataKat(kate1, urle1, bentuke1, kate2, urle2, bentuke2, cekIDe, namae, gambare, codeGames)
+function loopDataKat(kat1, url1, bentuk1, kat2, url2, bentuk2, kode, nama, gambar, codeGame) {
+  $("#1InputGame").val("")
+  $("#2InputGame").val("")
+  $("#genshinGame").val("")
+  $("#ragnarokGame").val("")
+
+  $("#imgOperator").attr("src", gambar)
+  $("#namaOperator").html(nama)
+  $("#dataPilihan").html("")
+  if (url2 == "" || url2 == "#") {
+    $("#dataPilihan").html(`<div class="container btn-pulsa text-center" id="tombol_${kat1}" onclick="getData('${url1}', '${kat1}', '${bentuk1}', '${kode}', '${codeGame}')" > 
+    <p class="p-1">${kat1}</p>
+    </div>`)
+  } else {
+    $("#dataPilihan").html(`<div class="container btn-pulsa text-center" id="tombol_${kat1}" onclick="getData('${url1}', '${kat1}', '${bentuk1}', '${kode}', '${codeGame}')"> 
+    <p class="p-1">${kat1}</p>
+    </div>
+    <div class="container btn-pulsa text-center" id="tombol_${kat2}" onclick="getData('${url2}', '${kat2}', '${bentuk2}', '${kode}', '${codeGame}')">
+    <p class="p-1">${kat2}</p>
+    </div> `)
+  }
+
+}
+
+
+
+
+
+
+// Loop Produk
+function loopProduk(data, kode, codeGame, bentuk) {
+  $("#dataProduk").html("")
+
+  $.each(data, function (i, v) {
+    var strHarga = v.harga.split("Rp")
+    var strProd = strHarga[1]
+    var hargaProd = `Rp ${strProd}`
+    if (bentuk == "list") {
+      $('#dataProduk').append(`<div class="cerd" style="margin-bottom:20px" data-bs-toggle="offcanvas" data-bs-target="#Id3" aria-controls="Id3" onclick="showDetail('${v.nama}','${hargaProd}','${v.url}','${kode}', '${codeGame}')">
+        <p class="nama-produk">${v.nama}</p>
+        <p class="harga-produk">${hargaProd}</p>
+        </div>`)
+    } else if (bentuk == "grid") {
+      $('#dataProduk').append(`<div class="col-6" style="margin-bottom:20px" data-bs-toggle="offcanvas" data-bs-target="#Id3" aria-controls="Id3" onclick="showDetail('${v.nama}','${hargaProd}','${v.url}','${kode}', '${codeGame}')">
+        <div class="cerd">
+        <p class="nama-produk">${v.nama}</p>
+        <p class="harga-produk">${hargaProd}</p>
+        </div>
+        </div>`)
+    }
+  });
+}
+
+
+
+// cekID
+function cekNomorPel(kode, codeGame) {
+  var urlz = "https://nielz.my.id/validation/api/game"
+  var zenkey = "8272b58207a73d9"
+  var firstInp = $("#1InputGame").val()
+  var secInp = $("#2InputGame").val()
+  var thirdInp = $("#genshinGame").val()
+  var fourthInp = $("#ragnarokGame").val()
+
+  let servering, isiServering, thirdInpz, fourthInpz, idGame
+
+  const getIndex = firstInp.indexOf("#")
+  const hsl = replaceChar(firstInp, getIndex, encodeURIComponent("#"))
+
+  if (codeGame == "pagarGame") {
+    idGame = hsl
+  } else {
+    idGame = firstInp
+  }
+
+  if (kode == "mobilelegends") {
+    servering = "&zone="
+  } else {
+    servering = "&server="
+  }
+
+  if (thirdInp == null) {
+    thirdInpz = "#"
+  } else {
+    thirdInpz = thirdInp
+  }
+
+  if (fourthInp == null) {
+    fourthInpz = "#"
+  } else {
+    fourthInpz = fourthInp
+  }
+
+  if (codeGame == "2InputGame") {
+    isiServering = secInp
+  } else if (codeGame == "genshinGame") {
+    isiServering = thirdInpz
+  } else if (codeGame == "ragnarokGame") {
+    isiServering = fourthInpz
+  }
+
+  if (codeGame == "1InputGame") {
+    $.ajax({
+      method: "GET",
+      url: `${urlz}/${kode}/?id=${idGame}&key=${zenkey}`,
+      beforeSend: function () {
+        $('#nama').html('Mengecek...')
+        $("#btls").removeClass("d-flex")
+        $("#btnMT").show()
+        $("#btnBli").hide()
+        $("#btnBtl").hide()
+      },
+
+      success: function (response) {
+        if (response.hasOwnProperty('error_msg')) {
+          $('#nama').html(response.error_msg);
+          $("#btls").removeClass("d-flex")
+          $("#btnBli").hide()
+          $("#btnMT").hide()
+          $("#btnBtl").show()
+        }
+
+        else {
+          $('#nama').html(response.nickname);
+          $("#btls").addClass("d-flex")
+          $("#btnBli").show()
+          $("#btnBtl").show()
+          $("#btnMT").hide()
+        }
+      }
+    })
+  } else if (codeGame == "pagarGame") {
+    $.ajax({
+      method: "GET",
+      url: `${urlz}/${kode}/?id=${idGame}&key=${zenkey}`,
+      beforeSend: function () {
+        $('#nama').html('Mengecek...')
+        $("#btls").removeClass("d-flex")
+        $("#btnMT").show()
+        $("#btnBli").hide()
+        $("#btnBtl").hide()
+      },
+
+      success: function (response) {
+        if (response.hasOwnProperty('error_msg')) {
+          $('#nama').html(response.error_msg);
+          $("#btls").removeClass("d-flex")
+          $("#btnBli").hide()
+          $("#btnMT").hide()
+          $("#btnBtl").show()
+        }
+
+        else {
+          $('#nama').html(response.nickname);
+          $("#btls").addClass("d-flex")
+          $("#btnBli").show()
+          $("#btnBtl").show()
+          $("#btnMT").hide()
+        }
+      }
+    })
+  } else {
+    $.ajax({
+      method: "GET",
+      url: `${urlz}/${kode}/?id=${idGame}${servering}${isiServering}&key=${zenkey}`,
+      beforeSend: function () {
+        $('#nama').html('Mengecek...')
+        $("#btls").removeClass("d-flex")
+        $("#btnMT").show()
+        $("#btnBli").hide()
+        $("#btnBtl").hide()
+      },
+
+      success: function (response) {
+        if (response.hasOwnProperty('error_msg')) {
+          $('#nama').html(response.error_msg);
+          $("#btls").removeClass("d-flex")
+          $("#btnBli").hide()
+          $("#btnMT").hide()
+          $("#btnBtl").show()
+        }
+
+        else {
+          $('#nama').html(response.nickname);
+          $("#btls").addClass("d-flex")
+          $("#btnBli").show()
+          $("#btnBtl").show()
+          $("#btnMT").hide()
+        }
+      }
+    })
+  }
+
+}
+
+
+// Beli
+function tombolBeli(link, idGame, zone, thirdInp, fourthInp, codeGame) {
+  let serGen
+  if (thirdInp == "os_asia") {
+    serGen = "001"
+  } else if (thirdInp == "os_usa") {
+    serGen = "002"
+  } else if (thirdInp == "os_euro") {
+    serGen = "003"
+  } else if (thirdInp == "os_cht") {
+    serGen = "004"
+  }
+
+  if (codeGame == "pagarGame") {
+    $("#btnBli").attr("href", link + "?catatan=" + idGame)
+  } else if (codeGame == "1InputGame") {
+    $("#btnBli").attr("href", link + "?catatan=" + idGame)
+  } else if (codeGame == "2InputGame") {
+    $("#btnBli").attr("href", link + "?catatan=" + idGame + zone)
+  } else if (codeGame == "genshinGame") {
+    $("#btnBli").attr("href", link + "?catatan=" + serGen + idGame)
+  } else if (codeGame == "ragnarokGame") {
+    $("#btnBli").attr("href", link + "?catatan=" + idGame + fourthInp)
+  }
+}
+
+// search
+$(document).ready(function () {
+  $("#search").on("input", function () {
+    var value = $(this).val().toLowerCase();
+    $("section .cerd").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+
+  $("#src").on("input", function () {
+    var value = $(this).val().toLowerCase();
+    $("section .kerd").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+})
