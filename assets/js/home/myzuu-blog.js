dcc04c512bdaa685551b0ccb7112733024f45750
Myprofilee <andrep13.id@gmail.com>
2023-03-11 14:12:58 +0700

    first commit

--- assets/js/home/myzuu-blog.js
+++ assets/js/home/myzuu-blog.js
@@ -4,7 +4,7 @@ $(function () {
     bar += '<div class="card-blog">';
 
         bar += '<div class="card-body-blog">';
-            bar += '<img src="https://sc.myzuu.my.id/assets/img/slide/blog/1.png" alt="Myzuu" class="img-card-myzuu" />';
+            bar += '<img src="https://sc.myzuu.my.id/assets/img/info/blog/1.png" alt="Myzuu" class="img-card-myzuu" />';
             bar += '<div class="text-card-blog">';
                 bar += '<h2 class="title-blog">Pusat Bantuan</h2>';
                 bar += '<p class="paragraph-blog">';
@@ -15,11 +15,22 @@ $(function () {
         bar += '</div>';
 
         bar += '<div class="card-body-blog">';
-            bar += '<img src="https://sc.myzuu.my.id/assets/img/slide/blog/2.png" alt="Myzuu" class="img-card-myzuu" />';
+            bar += '<img src="https://sc.myzuu.my.id/assets/img/info/blog/2.png" alt="Myzuu" class="img-card-myzuu" />';
             bar += '<div class="text-card-blog">';
                 bar += '<h2 class="title-blog">Promo Voucher Favorit</h2>';
                 bar += '<p class="paragraph-blog">';
                     bar += 'Yuk Cek Berbagai Promo Menarik Di Aplikasi Myzuu Sekarang!';
+                bar += '</p>';
+                bar += '<a href="https://myzuu.bukaolshop.site/akun/page?chat" class="link-blog">Cek Disini!</a>';
+            bar += '</div>';
+        bar += '</div>';
+
+        bar += '<div class="card-body-blog">';
+            bar += '<img src="https://sc.myzuu.my.id/assets/img/info/blog/3.png" alt="Myzuu" class="img-card-myzuu" />';
+            bar += '<div class="text-card-blog">';
+                bar += '<h2 class="title-blog">Cara TopUp MyzuuPay</h2>';
+                bar += '<p class="paragraph-blog">';
+                    bar += 'Yuk Cek Cara TopUp Untuk MyzuuPay Disini!';
                 bar += '</p>';
                 bar += '<a href="https://myzuu.bukaolshop.site/akun/page?chat" class="link-blog">Cek Disini!</a>';
             bar += '</div>';
--- assets/js/home/myzuu-event.js
+++ assets/js/home/myzuu-event.js
@@ -3,15 +3,7 @@ $(function () {
 
     bar += '<a href="#home">';
         bar += '<button class="button-myzuu-produk2">';
-            bar += '<img src="https://sc.myzuu.my.id/assets/img/slide/event/1.png" alt="Myzuu">';
-            bar += '<p>Weeken Deal Promo Paket data Telkomsel</p>';
-            bar += '<h4>Klik, Punya Kode Voucher? Gunakan skrng</h4>';
-        bar += '</button>';
-    bar += '</a>';
-
-    bar += '<a href="#home">';
-        bar += '<button class="button-myzuu-produk2">';
-            bar += '<img src="https://sc.myzuu.my.id/assets/img/slide/event/1.png" alt="Myzuu">';
+            bar += '<img src="/assets/img/info/event/1.png" alt="Myzuu">';
             bar += '<p>Weeken Deal Promo Paket data Telkomsel</p>';
             bar += '<h4>Klik, Punya Kode Voucher? Gunakan skrng</h4>';
         bar += '</button>';
